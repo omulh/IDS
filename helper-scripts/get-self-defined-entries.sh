bf05e34576e795f6cad58ee46aad94b9ee69ebc9
Omar L. <github@olh.xyz>
2024-10-15 14:40:28 +0200

    feat: give more useful output in get-self-defined-entries helper script

--- helper-scripts/get-self-defined-entries.sh
+++ helper-scripts/get-self-defined-entries.sh
@@ -6,6 +6,5 @@ IDS_FILE='../IDS.TXT'
 # composition option is the character itself
 selfDefinedChars=$(sed -n "/^[^\t]*\t\(.\)\t^\1/p" "$IDS_FILE")
 
-# Remove the text before and after the entry's
-# character for the remaining lines and output
-echo "$selfDefinedChars" | sed "s/^[^\t]*\t//" | sed -r "s/^(.).*/\1/"
+# Format and show the output
+echo "$selfDefinedChars" | sed "s/^\([^\t]*\)\t\(.\).*/\2(\1)/"
